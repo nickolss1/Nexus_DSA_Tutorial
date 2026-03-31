66612a96721a5dd433742b27e904e4635c15d1d1
nickolss1 <nuhamindereje2@gmail.com>
2026-03-31 13:47:01 +0300

    Create Toelitz.py

---
+++ Toelitz.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isToeplitzMatrix(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: bool
+        """
+        rows = len(matrix)
+        cols = len(matrix[0])
+        
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if matrix[i][j] != matrix[i-1][j-1]:
+                    return False
+        return True
