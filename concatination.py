48618c8ff0a8035bc31c1ad403346a51cc33d7fc
nickolss1 <nuhamindereje2@gmail.com>
2026-03-31 14:32:18 +0300

    Create concatination.py

---
+++ concatination.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = []
+        n = len(nums)
+        for i in range(2 * n):
+            ans.append(nums[i % n])
+        return ans
